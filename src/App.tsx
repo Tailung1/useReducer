c179d0c9676d2c37a44525f193938a8d45b91d8e
Tailung1 <ramazsamitashvili11@gmail.com>
2025-03-06 12:16:40 +0400

    add payload functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import "./App.css";
-import { useState, ChangeEvent,useReducer } from "react";
+import { useState, ChangeEvent, useReducer } from "react";
 
-function reducer(state,action) {
-    if(action.type==="inc") {
-        return state+1
-    }
-   if (action.type === "dec") {
-     return state - 1;
-   }
+function reducer(state, action) {
+  if (action.type === "inc") {
+    return state + action.payload;
+  }
+  if (action.type === "dec") {
+    return state - action.payload;
+  }
+  if (action.type === "setCount") {
+    return action.payload;
+  }
+  if (action.type === "reset") {
+    return 0;
+  }
 }
 
 function App() {
-    const initialState=0;
-  const [count,dispatch] = useReducer(reducer,initialState);
+  const initialState = 0;
+  const [count, dispatch] = useReducer(reducer, initialState);
 
-//   const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
   const date = new Date("2021-09-01");
@@ -26,19 +32,25 @@ function App() {
 
   function defineCount(event: ChangeEvent<HTMLInputElement>) {
     // setCount(Number(event.target.value));
+    dispatch({
+      type: "setCount",
+      payload: Number(event.target.value),
+    });
   }
 
   function inc() {
-    dispatch({type:"inc"})
+    dispatch({ type: "inc", payload: step });
   }
 
   function dec() {
-    dispatch({type:"dec"})
+    dispatch({ type: "dec", payload: step });
   }
 
   function reset() {
     // setCount(0);
-    setStep(0);
+    // setStep(0);
+    dispatch({ type: "reset" });
+    setStep(1);
   }
 
   return (
